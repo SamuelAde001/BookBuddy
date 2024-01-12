6fd852ec9edb37e419cf10ff9ea042cb7a8b6d51
Samuel Adebayo <repzysam@gmail.com>
2024-01-12 17:25:17 +0100

    updated helper

--- client/utils/helper.js
+++ client/utils/helper.js
@@ -1,1 +1,4 @@
-export const baseUrl = "http://localhost:3000";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://bookbuddy-qwvf.onrender.com"
+    : "http://localhost:3000";
